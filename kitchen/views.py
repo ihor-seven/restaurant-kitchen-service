63b28a856167d5014d3401e3ebaa1d88fc5b24a0
ihor-seven <kryskoihor@gmail.com>
2026-06-02 11:25:31 +0300

    Add CRUD views and templates for Dish and Cook

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,22 +1,65 @@
-from django.shortcuts import render
-from django.views import generic
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from django.urls import reverse_lazy
 from kitchen.models import Dish, Cook
 
-def index(request):
-    return render(request, "kitchen/index.html")
 
-class DishListView(generic.ListView):
+class DishListView(ListView):
     model = Dish
     template_name = "kitchen/dish_list.html"
 
-class DishDetailView(generic.DetailView):
+
+class DishDetailView(DetailView):
     model = Dish
     template_name = "kitchen/dish_detail.html"
 
-class CookListView(generic.ListView):
+
+class DishCreateView(CreateView):
+    model = Dish
+    fields = ["name", "description", "price", "dish_type", "cooks"]
+    template_name = "kitchen/dish_form.html"
+    success_url = reverse_lazy("dish-list")
+
+
+class DishUpdateView(UpdateView):
+    model = Dish
+    fields = ["name", "description", "price", "dish_type", "cooks"]
+    template_name = "kitchen/dish_form.html"
+    success_url = reverse_lazy("dish-list")
+
+
+class DishDeleteView(DeleteView):
+    model = Dish
+    template_name = "kitchen/dish_confirm_delete.html"
+    success_url = reverse_lazy("dish-list")
+
+
+class CookListView(ListView):
     model = Cook
     template_name = "kitchen/cook_list.html"
 
-class CookDetailView(generic.DetailView):
+
+class CookDetailView(DetailView):
     model = Cook
     template_name = "kitchen/cook_detail.html"
+
+
+class CookCreateView(CreateView):
+    model = Cook
+    fields = ["username", "first_name", "last_name", "years_of_experience"]
+    template_name = "kitchen/cook_form.html"
+    success_url = reverse_lazy("cook-list")
+
+
+class CookUpdateView(UpdateView):
+    model = Cook
+    fields = ["username", "first_name", "last_name", "years_of_experience"]
+    template_name = "kitchen/cook_form.html"
+    success_url = reverse_lazy("cook-list")
+
+
+class CookDeleteView(DeleteView):
+    model = Cook
+    template_name = "kitchen/cook_confirm_delete.html"
+    success_url = reverse_lazy("cook-list")
